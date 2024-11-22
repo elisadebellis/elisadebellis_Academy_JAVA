05e055b92aedd9f7d1dd01eee1660d13c696ee6c
elisadebellis <elisadebellis17@gmail.com>
2024-11-22 15:05:53 +0100

    innerclass

--- Java/22-11/TryCatch.java
+++ Java/22-11/TryCatch.java
@@ -22,6 +22,7 @@ public class TryCatch {
           static void checkAge(int age) {
             if (age < 18) {
                 // creare errore personalizzato 
+                //famiglie di errori: ArithmeticException, FileNotFoundException, ArrayIndexOutOfBoundsException, SecurityException
               throw new ArithmeticException("Access denied - You must be at least 18 years old.");}
             else {
               System.out.println("Access granted - You are old enough!"); }}
