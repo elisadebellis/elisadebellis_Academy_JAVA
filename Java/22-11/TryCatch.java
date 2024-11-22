f883c28b94abef8d6a3a9167bf0df42ec849f526
elisadebellis <elisadebellis17@gmail.com>
2024-11-22 14:32:05 +0100

    try/catch

--- Java/22-11/TryCatch.java
+++ Java/22-11/TryCatch.java
@@ -2,15 +2,28 @@ public class TryCatch {
 
     public static void main(String[ ] args) {
   
-      try {
-  
-        int[] myNumbers = {1, 2, 3, 4};
-  
-        System.out.println(myNumbers[12]);
-        System.out.println(myNumbers[3]);
-  
-      } catch (Exception e) {
-  
-        System.out.println("Qualcosa non è andato.");
-  
-    }}}
+        try {
+
+            int[] myNumbers = {1, 2, 3, 4};
+      
+            System.out.println(myNumbers[12]);
+      
+          } catch (Exception e) {
+      
+            System.out.println("Qualcosa non è andato.");
+            //viene eseguito anche se entra nel catch
+          } finally {
+      
+            System.out.println("il 'try catch' è finito.");
+      
+          }}
+
+
+          static void checkAge(int age) {
+            if (age < 18) {
+                // creare errore personalizzato 
+              throw new ArithmeticException("Access denied - You must be at least 18 years old.");}
+            else {
+              System.out.println("Access granted - You are old enough!"); }}
+ 
+}
